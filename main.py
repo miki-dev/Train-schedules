66c653650a1a45d67b1ca93585b55ba71a8859d9
miki-dev <67082479+miki-dev@users.noreply.github.com>
2020-06-18 14:27:24 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+# https://towardsdatascience.com/how-to-web-scrape-with-python-in-4-minutes-bc49186a8460
+#https://opensource.com/article/20/5/web-scraping-python
+
+#import libraries
 import requests
 import urllib.request
 import time
@@ -7,7 +11,8 @@ import pandas as pd
 
 #set webpage you want to search
 url = "https://www.jreast-timetable.jp/2006/timetable/tt1341/1341010.html"
-response = requests.get(url)
+page = requests.get(url)
+soup = BeautifulSoup(page.text, “html.parser”)
 
 station_name = []
 train_line = []
@@ -17,4 +22,6 @@ hours = []
 minutes = []
 n_of_trains = []
 
-# https://towardsdatascience.com/how-to-web-scrape-with-python-in-4-minutes-bc49186a8460
+#find time table
+timetable= soup.find_all(class_ = "timetable_box")
+
