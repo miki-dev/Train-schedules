442b98f9568ec5c387d6f879b4ea7d3955d28018
miki-dev <67082479+miki-dev@users.noreply.github.com>
2020-06-18 15:35:11 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 # https://towardsdatascience.com/how-to-web-scrape-with-python-in-4-minutes-bc49186a8460
-#https://opensource.com/article/20/5/web-scraping-python
+# https://opensource.com/article/20/5/web-scraping-python
+# https://www.crummy.com/software/BeautifulSoup/bs4/doc/
 
 #import libraries
 import requests
-#import urllib.request
+import urllib.request
 import time
 from bs4 import BeautifulSoup
 from selenium import webdriver
@@ -13,6 +14,7 @@ import pandas as pd
 url = "https://www.jreast-timetable.jp/2006/timetable/tt1341/1341010.html"
 page = requests.get(url)
 soup = BeautifulSoup(page.text, 'html.parser')
+print(soup.prettify())
 
 station_name = []
 train_line = []
@@ -24,7 +26,8 @@ n_of_trains = []
 
 #find time table
 timetable= soup.find_all(class_ = "timetable_box")
-
+for i in timetable:
+    
 
 # find all hours
 hours_list = soup.find_all(class_ = "results_03")
